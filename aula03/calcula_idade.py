807f5f1f8c7489849b393b6338b9e03d5afb2221
Artur Raguse <artur.raguse21@gmail.com>
2025-03-27 20:28:21 -0300

    pushing it once again

--- aula03/calcula_idade.py
+++ aula03/calcula_idade.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
-data_nasc = input("Digite sua data de nascimento: (dd/mm/yyyy")
+data_nasc = input("Digite sua data de nascimento: (dd/mm/yyyy)")
 nascimento = datetime.strptime(data_nasc, "%d/%m/%Y")
 hoje = datetime.today()
 idade = hoje.year - nascimento.year
 
 
 print(f"Voce têm {idade} anos.")
+
+if idade>=18:
+    print("Pode tirar sua CNH")
+    
