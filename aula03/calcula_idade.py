c6c1a32af7cf8ad676d2a999ab009a103023c64d
Artur Raguse <artur.raguse21@gmail.com>
2025-03-27 20:16:22 -0300

    commiting 1 more time

--- aula03/calcula_idade.py
+++ aula03/calcula_idade.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-data_nasc = input("Digite sua data de nascimento: (dd/mm/yyyy")
+data_nasc = input("Digite sua data de nascimento: (dd/mm/yyyy)")
 nascimento = datetime.strptime(data_nasc, "%d/%m/%Y")
 hoje = datetime.today()
 idade = hoje.year - nascimento.year
